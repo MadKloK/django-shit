99cd41979186775f5aca8fa95c50eace86cb906a
MadKloK <madklok12@gmail.com>
2026-06-20 14:15:11 +0330

    a lot of stuff happend, base template and stuff

--- app1/views.py
+++ app1/views.py
@@ -8,7 +8,10 @@ def json_test(request):
     return JsonResponse({'objective': 'test', 'damn': True})
 
 def home_view(request):
-    return render(request, 'app1/home.html')
+    return render(request, 'app1/index.html')
 
 def about_view(request):
     return render(request, 'app1/about.html')
+
+def contact_view(request):
+    return render(request, "app1/contact.html")
