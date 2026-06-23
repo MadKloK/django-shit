2bfebaa0316cbd0b54668025eca65f0475fdc7d6
MadKloK <madklok12@gmail.com>
2026-06-23 13:58:37 +0330

    created Post model

--- app2/models.py
+++ app2/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # image
+    # author
+    # tag
+    # category
+    views_count = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(null=True)
