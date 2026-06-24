7f4d863f98f3ba68cb607bf13a7b5a475fb9c57a
MadKloK <madklok12@gmail.com>
2026-06-24 11:01:42 +0330

    add real content to blog home page

--- app2/views.py
+++ app2/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from app2.models import Post
 
 # Create your views here.
 
 def blog_view(request):
-    return render(request, "app2/blog-home.html")
+    posts = Post.objects.filter(status=True)
+    context = {'posts': posts}
+    return render(request, "app2/blog-home.html", context)
 
 def blog_single(request):
     return render(request, "app2/blog-single.html")
