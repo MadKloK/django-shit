9f5624cc1934ea61fc6915e19917a307ae828983
MadKloK <madklok12@gmail.com>
2026-06-24 13:09:19 +0330

    damn views changed, update blog_view

--- app2/views.py
+++ app2/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 from app2.models import Post
 
 # Create your views here.
 
 def blog_view(request):
-    posts = Post.objects.filter(status=True)
+    posts = Post.objects.filter(published_at__lte=timezone.now(), status=True)
     context = {'posts': posts}
     return render(request, "app2/blog-home.html", context)
 
 def blog_single(request):
     return render(request, "app2/blog-single.html")
+
+def test(request, pid):
+    post = get_object_or_404(Post, id=pid)
+    context = {'post': post}
+
+    post.views_count += 1
+    post.save()
+
+    return render(request, 'app2/test.html', context)
