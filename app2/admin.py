805026f14a96567b5c5acc5113ffbe125cdb7660
MadKloK <madklok12@gmail.com>
2026-06-25 11:29:19 +0330

    add author to post model from django User model

--- app2/admin.py
+++ app2/admin.py
@@ -8,7 +8,7 @@ class App2Admin(admin.ModelAdmin):
     date_hierarchy = 'published_at'
     empty_value_display = 'this is empty'
     # fields = ['title', 'status']
-    list_display = ['__str__', 'status', 'created_at', 'updated_at']
-    list_filter = ['status', 'created_at']
+    list_display = ['__str__', 'author', 'status', 'created_at', 'updated_at']
+    list_filter = ['status', 'author', 'created_at']
     search_fields = ['title', 'content']
     ordering = ['created_at']
--- app2/models.py
+++ app2/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     # image
-    # author
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     # tag
     # category
     views_count = models.IntegerField(default=0)
