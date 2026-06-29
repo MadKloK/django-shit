3b348a82a2278389b90b936973b046372ec5221c
MadKloK <madklok12@gmail.com>
2026-06-29 16:00:08 +0330

    add dynamic lastest posts to main page

--- app1/views.py
+++ app1/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.core.paginator import Paginator
+from django.utils import timezone
 from app2.models import Post
 
 def home_view(request):
-    return render(request, 'app1/index.html')
+    posts = Post.objects.filter(
+        status=True,
+        published_at__lte=timezone.now()
+        ).order_by('-published_at')[:6].prefetch_related('category') # prefetch because of the query for categories in the template
+
+    context = {'posts': posts}
+    return render(request, 'app1/index.html', context)
 
 def about_view(request):
     return render(request, 'app1/about.html')
