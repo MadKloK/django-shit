b3f7b5a335616c236e7ce1c2c5f89c7930d84321
MadKloK <madklok12@gmail.com>
2026-06-29 11:50:04 +0330

    add search bar functionality at 'blog/search/?s= '

--- app2/views.py
+++ app2/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, get_object_or_404
-from django.db.models import F
+from django.db.models import F, Q
 from django.utils import timezone
 from app2.models import Post
 
 # Create your views here.
 
-def blog_view(request):
+def blog_view(request, **kwargs):
     posts = Post.objects.filter(published_at__lte=timezone.now(), status=True)
+
+    if kwargs.get('cat_name'):
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username'):
+        posts = posts.filter(author__username=kwargs['author_username'])
+
     context = {'posts': posts}
     return render(request, "app2/blog-home.html", context)
 
@@ -29,10 +35,20 @@ def blog_single(request, pid):
 
     return render(request, "app2/blog-single.html", context)
 
-def blog_category(request, cat_name):
-    posts = Post.objects.filter(status=True, category__name=cat_name)
+def blog_search(request):
+    posts = Post.objects.filter(published_at__lte=timezone.now(), status=True)
+
+    if s := request.GET.get('s'):
+        posts = posts.filter(Q(title__contains=s) | Q(content__contains=s))
+    
     context = {'posts': posts}
     return render(request, 'app2/blog-home.html', context)
+
+
+# def blog_category(request, cat_name):
+#     posts = Post.objects.filter(status=True, category__name=cat_name)
+#     context = {'posts': posts}
+#     return render(request, 'app2/blog-home.html', context)
 
 # def test(request, pid):
 #     post = get_object_or_404(Post, id=pid)
