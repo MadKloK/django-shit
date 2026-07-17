f01dc33359fa07eea8a7cc0373f2625b3feeeea2
MadKloK <madklok12@gmail.com>
2026-07-17 10:02:51 +0330

    settings for prod getting ready

---
+++ blog/setting/dev.py
@@ -0,0 +1,42 @@
+from blog.settings import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-@6r^fze%ln43hwd0t!3(4$9(v(k5(+24ru6iky1^!-lg74q5-4'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# sites framework
+SITE_ID = 1
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "assets",
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# debug toolbar
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
